d92c794ecdc4085854d3f8e4fe58a1d7f3df8d8e
wood08 <wood08@hotmail.com>
2019-05-11 16:02:06 +0900

    생활코딩 React 15. State

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import Content from './components/Content';
 import Subject from './components/Subject';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            subject:{title:"WEB", sub:"World Wide Wed!"}
+            ,contents:[
+                {id:1, title:"HTML", desc:"HTML is for information"}
+                ,{id:2, title:"CSS", desc:"CSS is for design"}
+                ,{id:3, title:"JavaScript", desc:"JavaScript is for interactive"}
+            ]
+        };
+    }
     render() {
         return (
             <div className="App">
-                <Subject title="WEB" sub="world wide web!"></Subject>
-                <Subject title="React" sub="For UI"></Subject>
-                <TOC></TOC>
+                <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+                <TOC data={this.state.contents}></TOC>
                 <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
             </div>
         );
