4e1aaccae06f278ff16c6bdfd93afe677cc7f532
wood08 <wood08@hotmail.com>
2019-05-12 12:49:04 +0900

    생활코딩 React 16. 이벤트

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            subject:{title:"WEB", sub:"World Wide Wed!"}
+            mode:"read"
+            ,subject:{title:"WEB", sub:"World Wide Wed!"}
+            ,welcome:{title:"Welcome", desc:"Hello React"}
             ,contents:[
                 {id:1, title:"HTML", desc:"HTML is for information"}
                 ,{id:2, title:"CSS", desc:"CSS is for design"}
@@ -17,11 +19,29 @@ class App extends Component {
         };
     }
     render() {
+        console.log("app render");
+        let _title, _desc = null;
+        if(this.state.mode === "welcome"){
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if(this.state.mode === "read"){
+            _title = this.state.contents[0].title;
+            _desc = this.state.contents[0].desc;
+        }
         return (
             <div className="App">
-                <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+                {/*<Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>*/}
+                <header>
+                    <h1><a href="/" onClick={function(e){
+                        console.log(e);
+                        e.preventDefault(); // a 태그 이벤트의 기본동작 막기
+                        this.setState({mode:"welcome"});
+                    }.bind(this)    // function 안에서 this 값을 쓸 수 있도록 추가
+                    }>{this.state.subject.title}</a></h1>
+                    {this.state.subject.sub}
+                </header>
                 <TOC data={this.state.contents}></TOC>
-                <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+                <Content title={_title} desc={_desc}></Content>
             </div>
         );
     }
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 
 class Subject extends Component {
     render() {
+        console.log("subject rander");
         return (
             <header>
-                <h1>{this.props.title}</h1>
+                <h1><a href="/">{this.props.title}</a></h1>
                 {this.props.sub}
             </header>
         );
